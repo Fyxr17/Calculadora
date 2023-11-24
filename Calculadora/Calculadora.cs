d0b04fb30c1a8a33ec770b3189e08989b36bf895
Marcos <ogandogabriel31@gmail.com>
2023-11-24 19:58:45 -0400

    Add project files.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        private double valor1;
+        private double valor2;
+
+        private double resultado;
+
+        private int operacion;
+
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //numero 3
+            tbDisplay.Text += "3";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //numero 2
+            tbDisplay.Text += "2";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //numero 9
+            tbDisplay.Text += "9";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //boton Punto
+            tbDisplay.Text = tbDisplay.Text + ".";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //numero 1
+            tbDisplay.Text += "1";
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            //boton Division
+            operacion = 4;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            //numero 0
+            tbDisplay.Text += "0";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //numero 4
+            tbDisplay.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            //numero 5
+            tbDisplay.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            //numero 6
+            tbDisplay.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            //numero 7
+            tbDisplay.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            //numero 8
+            tbDisplay.Text += "8";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //boton Limpiar
+            tbDisplay.Text = "";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            //boton Igual
+            valor2 = Convert.ToDouble(tbDisplay.Text);
+
+            switch (operacion)
+            {
+                case 1:
+                    resultado = valor1 + valor2;
+                    break;
+                case 2:
+                    resultado = valor1 - valor2;
+                    break;
+                case 3:
+                    resultado = valor1 * valor2;
+                    break;
+                case 4:
+                    resultado = valor1 / valor2;
+                    break;
+            }
+
+            tbDisplay.Text = resultado.ToString();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            //boton Suma
+            operacion = 1;
+            valor1 =Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            // boton Resta
+            operacion = 2;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            //boton Multiplicacion
+            operacion = 3;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+    }
+}
